bdb80b531590bcf77148971b9fc040ed44460500
Nephael <67867993+Nephael@users.noreply.github.com>
2021-09-14 03:26:59 +0900

    Create 구구단(노가다ver).java

---
+++ src/구구단(노가다ver).java
@@ -0,0 +1,133 @@
+public class Main {
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        int three = 3;
+        int four = 4;
+        int five = 5;
+        int six = 6;
+        int seven = 7;
+        int eight = 8;
+        int nine = 9;
+
+        //1단
+        System.out.println("1단입니다.");
+
+        System.out.println("1 x 1 = "+ one * one);
+        System.out.println("1 x 2 = "+ one * two);
+        System.out.println("1 x 3 = "+ one * three);
+        System.out.println("1 x 4 = "+ one * four);
+        System.out.println("1 x 5 = "+ one * five);
+        System.out.println("1 x 6 = "+ one * six);
+        System.out.println("1 x 7 = "+ one * seven);
+        System.out.println("1 x 8 = "+ one * eight);
+        System.out.println("1 x 9 = "+ one * nine);
+
+        //2단
+        System.out.println("2단입니다.");
+
+        System.out.println("2 x 1 = "+ two * one);
+        System.out.println("2 x 2 = "+ two * two);
+        System.out.println("2 x 3 = "+ two * three);
+        System.out.println("2 x 4 = "+ two * four);
+        System.out.println("2 x 5 = "+ two * five);
+        System.out.println("2 x 6 = "+ two * six);
+        System.out.println("2 x 7 = "+ two * seven);
+        System.out.println("2 x 8 = "+ two * eight);
+        System.out.println("2 x 9 = "+ two * nine);
+
+        //3단
+        System.out.println("3단입니다.");
+
+        System.out.println("3 x 1 = "+ three * one);
+        System.out.println("3 x 2 = "+ three * two);
+        System.out.println("3 x 3 = "+ three * three);
+        System.out.println("3 x 4 = "+ three * four);
+        System.out.println("3 x 5 = "+ three * five);
+        System.out.println("3 x 6 = "+ three * six);
+        System.out.println("3 x 7 = "+ three * seven);
+        System.out.println("3 x 8 = "+ three * eight);
+        System.out.println("3 x 9 = "+ three * nine);
+
+         //4단
+         System.out.println("4단입니다.");
+
+        System.out.println("4 x 1 = "+ four * one);
+        System.out.println("4 x 2 = "+ four * two);
+        System.out.println("4 x 3 = "+ four * three);
+        System.out.println("4 x 4 = "+ four * four);
+        System.out.println("4 x 5 = "+ four * five);
+        System.out.println("4 x 6 = "+ four * six);
+        System.out.println("4 x 7 = "+ four * seven);
+        System.out.println("4 x 8 = "+ four * eight);
+        System.out.println("4 x 9 = "+ four * nine);
+
+          //5단
+        System.out.println("5단입니다.");
+
+        System.out.println("5 x 1 = "+ five * one);
+        System.out.println("5 x 2 = "+ five * two);
+        System.out.println("5 x 3 = "+ five * three);
+        System.out.println("5 x 4 = "+ five * four);
+        System.out.println("5 x 5 = "+ five * five);
+        System.out.println("5 x 6 = "+ five * six);
+        System.out.println("5 x 7 = "+ five * seven);
+        System.out.println("5 x 8 = "+ five * eight);
+        System.out.println("5 x 9 = "+ five * nine);
+
+         //6단
+        System.out.println("6단입니다.");
+
+        System.out.println("6 x 1 = "+ six * one);
+        System.out.println("6 x 2 = "+ six * two);
+        System.out.println("6 x 3 = "+ six * three);
+        System.out.println("6 x 4 = "+ six * four);
+        System.out.println("6 x 5 = "+ six * five);
+        System.out.println("6 x 6 = "+ six * six);
+        System.out.println("6 x 7 = "+ six * seven);
+        System.out.println("6 x 8 = "+ six * eight);
+        System.out.println("6 x 9 = "+ six * nine);
+
+          //7단
+        System.out.println("7단입니다.");
+
+        System.out.println("7 x 1 = "+ seven * one);
+        System.out.println("7 x 2 = "+ seven * two);
+        System.out.println("7 x 3 = "+ seven * three);
+        System.out.println("7 x 4 = "+ seven * four);
+        System.out.println("7 x 5 = "+ seven * five);
+        System.out.println("7 x 6 = "+ seven * six);
+        System.out.println("7 x 7 = "+ seven * seven);
+        System.out.println("7 x 8 = "+ seven * eight);
+        System.out.println("7 x 9 = "+ seven * nine);
+
+         //8단
+        System.out.println("8단입니다.");
+
+        System.out.println("8 x 1 = "+ eight * one);
+        System.out.println("8 x 2 = "+ eight * two);
+        System.out.println("8 x 3 = "+ eight * three);
+        System.out.println("8 x 4 = "+ eight * four);
+        System.out.println("8 x 5 = "+ eight * five);
+        System.out.println("8 x 6 = "+ eight * six);
+        System.out.println("8 x 7 = "+ eight * seven);
+        System.out.println("8 x 8 = "+ eight * eight);
+        System.out.println("8 x 9 = "+ eight * nine);
+
+          //9단
+        System.out.println("9단입니다.");
+
+        System.out.println("9 x 1 = "+ nine * one);
+        System.out.println("9 x 2 = "+ nine * two);
+        System.out.println("9 x 3 = "+ nine * three);
+        System.out.println("9 x 4 = "+ nine * four);
+        System.out.println("9 x 5 = "+ nine * five);
+        System.out.println("9 x 6 = "+ nine * six);
+        System.out.println("9 x 7 = "+ nine * seven);
+        System.out.println("9 x 8 = "+ nine * eight);
+        System.out.println("9 x 9 = "+ nine * nine);
+
+        System.out.println("모든 구구단을 출력했습니다.");
+
+    }
+}
