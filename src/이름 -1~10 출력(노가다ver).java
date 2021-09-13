79f1d92859182e13c01cdf3f22160ca9053ad626
Nephael <67867993+Nephael@users.noreply.github.com>
2021-09-14 03:26:08 +0900

    Create 이름 -1~10 출력(노가다ver).java

---
+++ src/이름 -1~10 출력(노가다ver).java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int one = 1;
+        int two = 2;
+        int three = 3;
+        int four = 4;
+        int five = 5;
+        int six = 6;
+        int seven = 7;
+        int eight = 8;
+        int nine = 9;
+        int ten = 10;
+
+
+        System.out.println("Nephael -" + one);
+        System.out.println("Nephael -" + two);
+        System.out.println("Nephael -" + three);
+        System.out.println("Nephael -" + four);
+        System.out.println("Nephael -" + five);
+        System.out.println("Nephael -" + six);
+        System.out.println("Nephael -" + seven);
+        System.out.println("Nephael -" + eight);
+        System.out.println("Nephael -" + nine);
+
+    }
+}
