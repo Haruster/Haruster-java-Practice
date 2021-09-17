d6e74db71e9aa80b2569b6d0f502c22be28e5f08
Nephael <67867993+Nephael@users.noreply.github.com>
2021-09-18 04:16:43 +0900

    Create 1부터 100까지의 짝수의 합.java

---
+++ src/1부터 100까지의 짝수의 합.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 0, result = 0;
+
+        while (n < 100) {
+            n = n + 2;
+            result = result + n;
+        }
+
+        System.out.println(result);
+
+    }
+}
