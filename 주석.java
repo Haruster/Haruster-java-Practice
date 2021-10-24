741058ee68d1764b88b80a8ef72161e2527fee89
Apasys <67867993+Apasys@users.noreply.github.com>
2021-10-24 22:38:56 +0900

    Add files via upload

---
+++ 주석.java
@@ -0,0 +1,11 @@
+public class java02 {
+    //주석 사용하기
+    public static void main(String[] args) {
+        // 싱글라인주석
+        /* 
+            멀티라인주석
+         */
+        
+        System.out.println("hello world");
+    }
+}
