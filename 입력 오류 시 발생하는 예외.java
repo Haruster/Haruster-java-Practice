f8d1e82d69fbdab7957c4f05cdd01937dbc811d4
Apasys <67867993+Apasys@users.noreply.github.com>
2021-10-24 22:37:24 +0900

    Add files via upload

---
+++ 입력 오류 시 발생하는 예외.java
@@ -0,0 +1,46 @@
+import java.util.Scanner; // Scanner 모듈
+import java.util.InputMismatchException; // 예외 처리 관련 모듈
+
+public class main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("정수 3개를 입력하세요");
+
+        int sum = 0, n = 0;
+
+        for (int i = 0; i < 3; i++) {
+
+            System.out.println(i + ">>");
+
+            try {
+
+                n = scanner.nextInt(); // 정수 입력
+
+            }
+            catch(InputMismatchException e) {
+
+                System.out.println("정수가 아닙니다. 다시 입력하세요!!");
+
+                scanner.nextLine(); // 현재 스트림에 남아 있는 토큰을 지운다.
+
+                i--; // 인덱스가 증가하지 않도록 미리 감소한다.
+
+                continue; // 다음 루프
+
+            }
+
+            sum += n; //합하기
+
+        }
+
+        System.out.println("합은 " + sum);
+
+        scanner.close();
+
+
+    }
+
+}
