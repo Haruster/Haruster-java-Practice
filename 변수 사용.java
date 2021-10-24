cf1b5e4091c1ed8d96b22e9b27f6c9eefbcbb4f3
Apasys <67867993+Apasys@users.noreply.github.com>
2021-10-24 22:38:44 +0900

    Add files via upload

---
+++ 변수 사용.java
@@ -0,0 +1,11 @@
+public class value {
+    public static void main(String[] args) {
+        int i = 0;
+        
+        System.out.println("Hello world * " + i + "입니다.");
+        
+        i = i + i;
+        
+        System.out.println("Hello World *" + i + "입니다.");
+    }
+}
