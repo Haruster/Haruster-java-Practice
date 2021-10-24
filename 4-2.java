a1167faafb2495e523185c3b57ace97992b44183
Apasys <67867993+Apasys@users.noreply.github.com>
2021-10-24 22:31:34 +0900

    Add files via upload

---
+++ 4-2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class Rectangle {
+
+    int width; // 너비 필드
+    int height; // 높이 필드
+    
+    public int getArea() {
+
+        return width * height;
+
+    }
+
+}
+
+public class main {
+
+    public static void main(String[] args) {
+
+        Rectangle rect = new Rectangle(); // 객체 생성
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print(">> ");
+
+        rect.width = scanner.nextInt();
+        rect.height = scanner.nextInt();
+
+        System.out.println("사각형의 면적은 " + rect.getArea());
+
+        scanner.close();
+
+    }
+
+}
