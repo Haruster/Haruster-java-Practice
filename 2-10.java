6af2824b28343a6fb30ecaca6ae96e0fc02a7e55
Apasys <67867993+Apasys@users.noreply.github.com>
2021-10-24 22:27:58 +0900

    Add files via upload

---
+++ 2-10.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("점수를 입력하세요 : ");
+
+        int score = scanner.nextInt();
+
+        if (score >= 80)
+            System.out.println("축하합니다 ! 합격입니다.");
+
+        scanner.close();
+    }
+}
