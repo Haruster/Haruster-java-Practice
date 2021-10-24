d39a6865f8f973d7a354cb4df0ecdea5dd90fb4d
Apasys <67867993+Apasys@users.noreply.github.com>
2021-10-24 22:38:49 +0900

    Add files via upload

---
+++ 사칙연산.java
@@ -0,0 +1,19 @@
+public class java {
+    //사칙연산
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        
+        int one = a + b;
+        int two = a - b;
+        int three = a * c;
+        int four =  d / c;
+        
+        System.out.println(one);
+        System.out.println(two);
+        System.out.println(three);
+        System.out.println(four);
+    }
+}
